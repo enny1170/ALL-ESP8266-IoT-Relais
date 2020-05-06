26e97213f221aa47b8d8a9cf8c90d6ea1da389d1
Ziehe Enrico <E.Ziehe@strube.net>
2020-05-06 09:11:29 +0200

    code cleanup, switch by http-get

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,8 @@
 #include <mqttconfig.h>
 #include <Ticker.h>
 #include <AsyncMqttClient.h>
+#include "webserver.h"
+#include "mqtthelper.h"
 
 #define SPIFFS_USE_MAGIC
 
@@ -34,13 +36,9 @@ wificonfig wifiConfig;
 String SubscribePath;
 String PublishPath;
 
-const char* PARAM_MESSAGE = "message";
+const char* PARAM_MESSAGE = "cmd";
 
-void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len);
 
-void notFound(AsyncWebServerRequest *request) {
-    request->send(404, "text/plain", "Not found");
-}
 
 #define MQTT_HOST IPAddress(192, 168, 1, 10)
 #define MQTT_PORT 1883
@@ -57,116 +55,6 @@ void switchCoil(bool on);
 bool GetState();
 const char* StateValue();
 
-void connectToMqtt() {
-  Serial.println("Connecting to MQTT...");
-  if(mqttcfg.Enabled)
-  {
-    mqttClient.connect();
-  }
-}
-
-void connectToWifi() {
-  Serial.println("Connecting to Wi-Fi...");
-//  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  wifiConfig.begin();
-}
-
-void onWifiConnect(const WiFiEventStationModeGotIP& event) {
-  Serial.println("Connected to Wi-Fi.");
-  SubscribePath=wifiConfig.Name+String("/cmd");
-  PublishPath=wifiConfig.Name+String("/state");
-  connectToMqtt();
-}
-
-void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
-  Serial.println("Disconnected from Wi-Fi.");
-  mqttReconnectTimer.detach(); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
-  wifiReconnectTimer.once(2, connectToWifi);
-}
-
-void onMqttConnect(bool sessionPresent) {
-  Serial.println("Connected to MQTT.");
-  Serial.print("Session present: ");
-  Serial.println(sessionPresent);
-//  uint16_t packetIdSub = mqttClient.subscribe("test/lol", 2);
-  Serial.print("Subscribe to: ");
-  Serial.println(SubscribePath);
-  mqttClient.publish(SubscribePath.c_str(),1,true, StateValue());
-  uint16_t packetIdSub = mqttClient.subscribe(SubscribePath.c_str(), 2);
-  Serial.print("Subscribing at QoS 2, packetId: ");
-  Serial.println(packetIdSub);
-//  mqttClient.publish("test/lol", 0, true, "test 1");
-  Serial.print("Publish at: ");
-  Serial.println(PublishPath);
-  mqttClient.publish(PublishPath.c_str(), 1, true, StateValue());
-  // Serial.println("Publishing at QoS 0");
-  // uint16_t packetIdPub1 = mqttClient.publish("test/lol", 1, true, "test 2");
-  // Serial.print("Publishing at QoS 1, packetId: ");
-  // Serial.println(packetIdPub1);
-  // uint16_t packetIdPub2 = mqttClient.publish("test/lol", 2, true, "test 3");
-  // Serial.print("Publishing at QoS 2, packetId: ");
-  // Serial.println(packetIdPub2);
-}
-
-void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
-  Serial.println("Disconnected from MQTT.");
-
-  if (WiFi.isConnected()) {
-    mqttReconnectTimer.once(2, connectToMqtt);
-  }
-}
-
-void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
-  Serial.println("Subscribe acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
-  Serial.print("  qos: ");
-  Serial.println(qos);
-}
-
-void onMqttUnsubscribe(uint16_t packetId) {
-  Serial.println("Unsubscribe acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
-}
-
-void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
-  Serial.println("Publish received.");
-  Serial.print("  topic: ");
-  Serial.println(topic);
-  Serial.print("  qos: ");
-  Serial.println(properties.qos);
-  Serial.print("  dup: ");
-  Serial.println(properties.dup);
-  Serial.print("  retain: ");
-  Serial.println(properties.retain);
-  Serial.print("  len: ");
-  Serial.println(len);
-  Serial.print("  index: ");
-  Serial.println(index);
-  Serial.print("  total: ");
-  Serial.println(total);
-  Serial.print(" load: ");
-  Serial.println(payload);
-  Serial.printf("Command received: %c",payload[index]);
-  if(payload[index]=='0' && GetState()!=false)
-  {
-    Serial.println("Switch to Off (0)");
-    switchCoil(false);
-  }
-  else if(payload[index]=='1' && GetState()!=true)
-  {
-    Serial.println("Switch to On (1)");
-    switchCoil(true);
-  }
-  //mqttClient.publish(PublishPath.c_str(), 1, true, StateValue());
-}
-
-void onMqttPublish(uint16_t packetId) {
-  Serial.println("Publish acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
-}
 
 String indexProcessor(const String& var);
 
@@ -222,33 +110,9 @@ void setup() {
     // Serial.print("IP Address: ");
     // Serial.println(WiFi.localIP());
 
-  wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
-  wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
+    registerMqttEvents();
 
-  mqttClient.onConnect(onMqttConnect);
-  mqttClient.onDisconnect(onMqttDisconnect);
-  mqttClient.onSubscribe(onMqttSubscribe);
-  mqttClient.onUnsubscribe(onMqttUnsubscribe);
-  mqttClient.onMessage(onMqttMessage);
-  mqttClient.onPublish(onMqttPublish);
-  mqttClient.setServer(mqttcfg.ServerAddress.c_str(), mqttcfg.ServerPort);
-
-    //Send OTA events to the browser
-    ArduinoOTA.onStart([]() { events.send("Update Start", "ota"); });
-    ArduinoOTA.onEnd([]() { events.send("Update End", "ota"); });
-    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-      char p[32];
-      sprintf(p, "Progress: %u%%\n", (progress/(total/100)));
-      events.send(p, "ota");
-    });
-    ArduinoOTA.onError([](ota_error_t error) {
-      if(error == OTA_AUTH_ERROR) events.send("Auth Failed", "ota");
-      else if(error == OTA_BEGIN_ERROR) events.send("Begin Failed", "ota");
-      else if(error == OTA_CONNECT_ERROR) events.send("Connect Failed", "ota");
-      else if(error == OTA_RECEIVE_ERROR) events.send("Recieve Failed", "ota");
-      else if(error == OTA_END_ERROR) events.send("End Failed", "ota");
-    });
-
+    setupOTA();
 
     Serial.println("Mounting FS...");
     if (!SPIFFS.begin()) {
@@ -261,16 +125,12 @@ void setup() {
     connectToWifi();
 
     MDNS.addService("http","tcp",80);
-    // ArduinoOTA.setHostname(wifiConfig.Name.c_str());
-    // ArduinoOTA.begin();
+    ArduinoOTA.setHostname(wifiConfig.Name.c_str());
+    ArduinoOTA.begin();
 
-    ws.onEvent(onWsEvent);
-    server.addHandler(&ws);
+    registerWebEvents();
 
-    events.onConnect([](AsyncEventSourceClient *client){
-      client->send("hello!",NULL,millis(),1000);
-    });
-    server.addHandler(&events);
+    // Register Wepages and Handler
 
     //Move client to /html/index.html
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
@@ -283,14 +143,37 @@ void setup() {
 
     server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
 
-    // Send a GET request to <IP>/get?message=<message>
-    server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    // Send a GET request to <IP>/html/switch?message=<message>
+    server.on("/html/switch", HTTP_GET, [] (AsyncWebServerRequest *request) {
         String message;
         if (request->hasParam(PARAM_MESSAGE)) {
             message = request->getParam(PARAM_MESSAGE)->value();
         } else {
-            message = "No message sent";
+            message = "";
         }
+        if(message!="0" && message!="1")
+        {
+          request->send(403,"text/plain","unknown or malformed parameter. ?cmd=1 or ?cmd=0");
+        }
+        else
+        {
+          if(message=="1" && GetState()==false)
+          {
+            switchCoil(true);
+            request->send(200,"text/plain","new State ON");
+          }
+          else if(message=="0" && GetState()==true)
+          {
+            switchCoil(false);
+            request->send(200,"text/plain","New State OFF");
+          }
+          else
+          {
+            request->send(201);
+          }
+          
+        }
+        
         request->send(200, "text/plain", "Hello, GET: " + message);
     });
 
@@ -316,77 +199,6 @@ void loop() {
 
 }
 
-void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
-  if(type == WS_EVT_CONNECT){
-    Serial.printf("ws[%s][%u] connect\n", server->url(), client->id());
-    client->printf("Hello Client %u :)", client->id());
-    client->ping();
-  } else if(type == WS_EVT_DISCONNECT){
-    Serial.printf("ws[%s][%u] disconnect\n", server->url(), client->id());
-  } else if(type == WS_EVT_ERROR){
-    Serial.printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t*)arg), (char*)data);
-  } else if(type == WS_EVT_PONG){
-    Serial.printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len)?(char*)data:"");
-  } else if(type == WS_EVT_DATA){
-    AwsFrameInfo * info = (AwsFrameInfo*)arg;
-    String msg = "";
-    if(info->final && info->index == 0 && info->len == len){
-      //the whole message is in a single frame and we got all of it's data
-      Serial.printf("ws[%s][%u] %s-message[%llu]: ", server->url(), client->id(), (info->opcode == WS_TEXT)?"text":"binary", info->len);
-
-      if(info->opcode == WS_TEXT){
-        for(size_t i=0; i < info->len; i++) {
-          msg += (char) data[i];
-        }
-      } else {
-        char buff[3];
-        for(size_t i=0; i < info->len; i++) {
-          sprintf(buff, "%02x ", (uint8_t) data[i]);
-          msg += buff ;
-        }
-      }
-      Serial.printf("%s\n",msg.c_str());
-
-      if(info->opcode == WS_TEXT)
-        client->text("I got your text message");
-      else
-        client->binary("I got your binary message");
-    } else {
-      //message is comprised of multiple frames or the frame is split into multiple packets
-      if(info->index == 0){
-        if(info->num == 0)
-          Serial.printf("ws[%s][%u] %s-message start\n", server->url(), client->id(), (info->message_opcode == WS_TEXT)?"text":"binary");
-        Serial.printf("ws[%s][%u] frame[%u] start[%llu]\n", server->url(), client->id(), info->num, info->len);
-      }
-
-      Serial.printf("ws[%s][%u] frame[%u] %s[%llu - %llu]: ", server->url(), client->id(), info->num, (info->message_opcode == WS_TEXT)?"text":"binary", info->index, info->index + len);
-
-      if(info->opcode == WS_TEXT){
-        for(size_t i=0; i < len; i++) {
-          msg += (char) data[i];
-        }
-      } else {
-        char buff[3];
-        for(size_t i=0; i < len; i++) {
-          sprintf(buff, "%02x ", (uint8_t) data[i]);
-          msg += buff ;
-        }
-      }
-      Serial.printf("%s\n",msg.c_str());
-
-      if((info->index + len) == info->len){
-        Serial.printf("ws[%s][%u] frame[%u] end[%llu]\n", server->url(), client->id(), info->num, info->len);
-        if(info->final){
-          Serial.printf("ws[%s][%u] %s-message end\n", server->url(), client->id(), (info->message_opcode == WS_TEXT)?"text":"binary");
-          if(info->message_opcode == WS_TEXT)
-            client->text("I got your text message");
-          else
-            client->binary("I got your binary message");
-        }
-      }
-    }
-  }
-}
 
 String indexProcessor(const String& var)
 {
